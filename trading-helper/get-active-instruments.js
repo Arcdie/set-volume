bf1d619e1bb2c77e22a27536ac6d9abd7c3a0b43
Valentyn <xdshkaa2@gmail.com>
2021-11-22 21:27:57 +0200

    master: changed url

--- trading-helper/get-active-instruments.js
+++ trading-helper/get-active-instruments.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const getActiveInstruments = async () => {
   const responseGetData = await axios({
     method: 'get',
-    url: 'http://91.240.242.90/api/instruments/active',
+    url: 'https://trading-helper.ru/api/instruments/active',
     headers: {
       'Content-Type': 'application/json',
     },
